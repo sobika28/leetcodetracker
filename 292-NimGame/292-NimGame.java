bebcd8712c2a5234ed23605c37139130683eb639
sobika28 <sobikashunmugom@gmail.com>
2026-08-11 12:23:06 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,9 @@
+// Last updated: 8/11/2026, 12:23:06 PM
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4!=0)
+           return true;
+        else
+            return false;
+    }
+}
